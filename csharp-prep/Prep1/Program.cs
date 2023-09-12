5a2b657555af99a651d31f5209a965b505a3c816
ttyybb-code <ttyybbcode@sudomail.com>
2023-09-12 14:12:27 -0600

    Fixed cammel case

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,9 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("What is your first name? ");
-        string FirstName = Console.ReadLine();
+        string firstName = Console.ReadLine();
         Console.Write("What is your last name? ");
-        string LastName = Console.ReadLine();
-        Console.WriteLine($"Your name is {LastName}, {FirstName} {LastName}");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
     }
 }
